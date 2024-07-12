9c487244b87156f808f17d5d279b6d17eef92cf2
Gaurav Joshi <gj451026@gmail.com>
2024-07-12 10:11:11 +0530

    New poetry added

--- src/PoetryShowcase.tsx
+++ src/PoetryShowcase.tsx
@@ -2,14 +2,21 @@ import React from "react";
 
 import "./PoetryShowcase.scss"
 
-import poetry_1 from "./poetry/poetry_1.png"
-import poetry_2 from "./poetry/poetry_2.png"
+import poetry_1 from "./poetry/Lose Her.png"
+import poetry_2 from "./poetry/2.png"
 import poetry_3 from "./poetry/poetry_3.png"
+import poetry_4 from "./poetry/Copy of Only girl I desire.png"
+import poetry_5 from "./poetry/It's not erased .png"
+import poetry_6 from "./poetry/Girl I Met.png"
+
 const PoetryShowcase: React.FC = () => {
   const poetryImages = [
     poetry_1,
     poetry_2,
     poetry_3,
+    poetry_4,
+    poetry_5,
+    poetry_6,
   ];
 
   return (
